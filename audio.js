b89507ea79b9411ef66a7854c49c7a6f90f12858
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2017-07-30 01:13:29 +0900

    アナライザー追加。とりあえずボリューム表示。

--- audio.js
+++ audio.js
@@ -11,14 +11,17 @@
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   var context = new AudioContext();
 
-  // 音源ノード生成（audio要素からオーディオデータ取得）
-  // audio要素を取得
+
+  //音源ノード生成（audio要素からオーディオデータ取得）
+  //audio要素を取得
   var audioSrc = document.getElementById("audio-element");
   // audio要素を音源とするMediaElementAudioSourceNodeを生成
   var audio = context.createMediaElementSource(audioSrc);
 
-  // 中間処理（音量調整処理）を表すAudioNodeが持つAudioParamに
-  // input要素の値を代入。表示にも値を反映する
+
+  //ゲイン
+  //中間処理（音量調整処理）を表すAudioNodeが持つAudioParamに
+  //input要素の値を代入。表示にも値を反映する
   var elementGain = document.getElementById('gain');
   var elementGainValue = document.getElementById('gain-value');
   var gain = context.createGain();
@@ -27,7 +30,8 @@
   };
   setGain();
 
-  //フィルター
+
+  //ローパスフィルター＆ハイパスフィルター
   var elementBiquadFilterType = document.getElementById('biquad-filter-type');
   var elementBiquadFilterFrequency = document.getElementById('biquad-filter-frequency');
   var elementBiquadFilterFrequencyValue = document.getElementById('biquad-filter-frequency-value');
@@ -63,10 +67,56 @@
   setBiquadFilterFrequency();
 
 
+  //なにも選ばないときはこれ。
   //音源ノード → GainNode
   audio.connect(gain);
   //Gainノード -> 音声出力ノード
   gain.connect(context.destination);
+
+
+  //AnalyserNodeオブジェクトを生成
+  var analyser = context.createAnalyser();
+  //解析対象の音声データを格納するための型付き配列を準備(Uint8Array:符号なし8bit整数の配列)
+  //引数(配列の要素数)にはAnalyserNodeオブジェクトのfrequencyBinCountプロパティを設定する
+  var frequencies = new Uint8Array(analyser.frequencyBinCount);
+  //音量として、解析結果の全周波数の振幅の平均を計算する
+  var getByteFrequencyDataAverage = function() {
+    //配列に周波数ごとの振幅を格納
+    analyser.getByteFrequencyData(frequencies);
+    //解析結果の全周波数の振幅を合計し要素数で割ることで平均を求める
+    return frequencies.reduce(function(previous, current) {
+      return previous + current;
+    }) / analyser.frequencyBinCount;
+  };
+
+
+  //マイク取得
+  //音声のみ取得する場合は引数を{audio: true}に設定する
+  //返り値はPromiseオブジェクト
+  navigator.mediaDevices.getUserMedia({audio: true})
+    // 成功ハンドラが引数としてMediaStreamオブジェクトを受け取る
+    .then(function(stream) {
+      // Firefoxには、グローバルに保持しておかないと
+      // MediaStreamオブジェクトへの参照が時間経過でなくなってしまうバグがあるため、対策しておく
+      window.hackForMozzila = stream;
+      // MediaStreamAudioSourceNodeを生成
+      // 引数にはMediaStreamオブジェクトが必要 アナライザーに接続
+      context.createMediaStreamSource(stream).connect(analyser);
+    });
+
+
+  //ボリューム表示
+  var draw;
+  var elementVolume = document.getElementById('volume');
+  //可能な限り高いフレームレートで音量を取得し、表示を更新する
+  (draw = function() {
+    elementVolume.innerHTML = Math.floor(getByteFrequencyDataAverage());
+    requestAnimationFrame(draw);
+  })();
+
+
+  var elementImg = document.getElementById('prfmMagicOfLove');
+
 
   // DOMへのイベント登録
   var elementButton = document.getElementById('button');
